537199c1cba86584da8e7ed36f8172e0c9e0950e
code-with-santanu <santanupal003@gmail.com>
2022-03-19 14:05:22 +0530

    changes in header file

--- main.h
+++ main.h
@@ -1,8 +1,8 @@
-int InsertInArray(int arr[]);
-void Print(int arr[], int length);
-void Reverse(int arr[], int length);
-int SearchFirstOccurrence(int arr[], int s, int length);
-int DeleteElementAtPosition(int arr[], int NewPos, int length);
-int SearchAndDelete(int arr, int s, int length, int oc);
-void ArrayModification(int arr[], int choice, int length);
-void Swap(int arr[], int left, int right);
+int InsertInArray(int);
+void Print(int, int);
+void Reverse(int, int);
+int SearchFirstOccurrence(int, int, int);
+int DeleteElementAtPosition(int, int, int);
+int SearchAndDelete(int, int, int, int);
+void ArrayModification(int, int, int);
+void Swap(int, int, int);
