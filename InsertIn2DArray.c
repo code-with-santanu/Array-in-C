bc72e35410d18f8c5d08b055867f578de72d8f4b
code-with-santanu <santanupal003@gmail.com>
2022-05-14 03:07:52 +0530

    Initial commit

---
+++ InsertIn2DArray.c
@@ -0,0 +1,12 @@
+void Insert(int array[20][20], int row, int col)
+{
+    int i, j;
+    printf("\nEnter the elements: ");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            scanf("%d", &array[i][j]);
+        }
+    }
+}
