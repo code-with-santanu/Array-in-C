fdc883fe98dfb6fcb5bf1da460cf99c5f0f9f9ea
code-with-santanu <santanupal003@gmail.com>
2022-05-14 13:42:51 +0530

    file name changed

--- MyLibrary.h
+++
@@ -1,9 +0,0 @@
-int InsertInArray(int arr[]);
-void Print(int arr[], int);
-void Reverse(int arr[], int);
-void Swap(int arr[], int, int);
-int SearchFirstOccurrence(int arr[], int, int);
-int DeleteElementAtPosition(int arr[], int, int);
-
-void Insert(int array[20][20], int, int);
-void Print2DArray(int array[20][20], int, int);
