965023a1a7a94387fe95014073d7b29f1dbed9cb
code-with-santanu <santanupal003@gmail.com>
2023-01-20 02:13:27 +0530

    main func is removed

--- SearchingAlgo.c
+++ SearchingAlgo.c
@@ -1,76 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
+/*=== Searching algorithm is used to search if the data is present in array or not.If found , return it's position, else -1 ===*/
 
-void InsertInArray(int arr[],int );
-void Print(int arr[],int );
-int LinearSearch(int arr[],int ,int );
-int BinarySearch(int arr[],int ,int );
-int InterpolationSearch(int arr[],int , int );
-
-
-int main()
-{
-    int *a;
-    int size,targetNo,searchIndex;
-
-    printf("\nEnter the size of the array: ");
-    scanf("%d",&size);
-    a=(int*)malloc(sizeof(int)*size);
-
-    InsertInArray(a,size);
-    Print(a,size);
-    printf("\nEnter a no from the array to search: ");
-    scanf("%d",&targetNo);
-
-
-    //choose any one algo and comment rest to algo before run the program
-    searchIndex=LinearSearch(a,size,targetNo);        
-    searchIndex=BinarySearch(a,size,targetNo);
-    searchIndex=InterpolationSearch(a,size,targetNo);
-
-    if(searchIndex==-1)
-    {
-        printf("\nThe no(%d) you are searching is not in the array.",targetNo);
-    }
-    else
-    {
-        printf("\nThe no(%d) you are searching is at %d th position in the array.",targetNo,searchIndex);
-    }
-
-    return 0;
-
-}
-
-
-void InsertInArray(int arr[],int size)
-{
-    int i,n;
-
-    for(i=0;i<size;i++)
-    {
-        printf("\nEnter a no: ");
-        scanf("%d",&n); 
-        arr[i]=n;   
-    }
-
-}
-
-void Print(int arr[],int size)
+// Best case- O(1)     worst case- O(n)
+int LinearSearch(int *arr,int size,int targetNo)
 {
     int i;
 
-    printf("\nThe array: ");
-    for(i=0;i<size;i++)
-    {
-        printf("%d\t",arr[i]);
-    }
-}
-
-int LinearSearch(int arr[],int size,int targetNo)
-{
-    int i;
-
-    for(i=0;i<size;i++)
+    for (i = 0; i < size; i++)
     {
         if(arr[i]==targetNo)
         {
@@ -83,7 +18,8 @@ int LinearSearch(int arr[],int size,int targetNo)
 
 
 //Only for sorted array and can't be used in linked list
-int BinarySearch(int arr[],int size,int targetNo)
+// Best case- O(1)     worst case- O(log(n))
+int BinarySearch(int *arr,int size,int targetNo)
 {
     int low,mid,high;
     low=0;
@@ -92,7 +28,7 @@ int BinarySearch(int arr[],int size,int targetNo)
     //keep searching until low and high converges
     while(low<=high)
     {
-        mid=(low+high)/2;
+        mid = (low + high) / 2;
         if(arr[mid]==targetNo)
         {
             return mid;
@@ -100,12 +36,12 @@ int BinarySearch(int arr[],int size,int targetNo)
         // If no is larger, no is in upper part
         else if(arr[mid] < targetNo)
         {
-            low=mid+1;
+            low = mid + 1;
         }
         //If no is smaller, no is in the lower part
         else
         {
-            high =mid-1;
+            high = mid - 1;
         }
     }
 
@@ -114,11 +50,12 @@ int BinarySearch(int arr[],int size,int targetNo)
 
 
 //Applicable for sorted array and data should be uniformly distributed
-int InterpolationSearch(int arr[],int size, int targetNo)
+// Best case- O(1)     worst case- O(log(log n))
+int InterpolationSearch(int *arr,int size, int targetNo)
 {
-    int low,pos,high;
+    int low, pos, high;
     low=0;
-    high=size-1;
+    high = size - 1;
 
     //for 1 ele present in array(to avoid zero devision error)
     if(size==1)
