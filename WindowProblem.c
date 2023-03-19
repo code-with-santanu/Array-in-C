ae45266d71f019d885d10720bff0fe27d4a369f1
code-with-santanu <santanupal003@gmail.com>
2023-03-19 21:26:03 +0530

    initial commit

---
+++ WindowProblem.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "ArrayLibrary.h"
+
+//compile with InsertAndPrintArray.c , SortingAlgo.c , Swap.c
+
+int Window(int arr[],int length, int key)
+{
+    int MinDiff, i, j, dif;
+
+    MinDiff = arr[length - 1] - arr[0];
+    i = 0;
+    while(i+key-1 < length)
+    {
+        j = i + key - 1;
+        
+        
+        dif = arr[i+key-1] - arr[i];
+
+        if (dif < MinDiff)
+        {
+            MinDiff = dif;
+        }
+        i++;
+    }
+    
+
+
+    return MinDiff;
+}
+
+int main()
+{
+    int *arr,length ,Mindifference,key;
+
+    printf("\nEnter the length of the array: ");
+    scanf("%d", &length);
+
+    arr = (int *)malloc(sizeof(int) * length);
+    InsertIn1DArray(arr, length);
+    printf("\nThe given array .\n");
+    Print1DArray(arr, length);
+
+    MergeSort(arr, 0, length-1);
+    
+    printf("\nEnter the value of key: ");
+    scanf("%d", &key);
+
+    Mindifference = Window(arr, length, key);
+
+    printf("\nThe minimum diffenrence is: %d", Mindifference);
+
+    return 0;
+}
